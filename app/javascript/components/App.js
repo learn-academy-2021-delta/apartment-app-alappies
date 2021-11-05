4d9ea528511aaf88be276376c13c51cfaf0c6106
alappie <ashley@specialtyproduce.com>
2021-11-05 16:57:01 -0700

    half way through this list

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import ApartmentIndex from './pages/ApartmentIndex'
+import Header from './components/Header'
 import Home from './pages/Home'
-import Footer from './Footer'
+import ApartmentIndex from './pages/ApartmentIndex'
+import Footer from './components/Footer'
 import ApartmentShow from './pages/ApartmentShow'
-import Header from './Header'
+
 
 import {
   BrowserRouter as Router, 
@@ -13,14 +14,13 @@ import {
 
 class App extends Component {
   render() {
-  
     return (    
   <>
   <Router>
-    <Header {...this.props}/>
+    <Header {...this.props} />
     <Routes>
     <Route exact path="/" element={ <Home /> } />
-    <Route path="/index" element={ <ApartmentIndex /> } />
+    <Route path="/apartmentindex" element={ <ApartmentIndex /> } />
     <Route path="/show" element={ <ApartmentShow /> } />
     </Routes>
     <Footer />
